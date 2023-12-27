b8780c2972ae2304b47e8f301ee6e25b9f75cd2d
Amir2417 <amirsohelto@gmail.com>
2023-12-27 09:29:18 +0600

    : receipient payment message update

--- app/Http/Controllers/Api/V1/User/SendRemittanceController.php
+++ app/Http/Controllers/Api/V1/User/SendRemittanceController.php
@@ -574,7 +574,9 @@ class SendRemittanceController extends Controller
         }
         $temporary_data     = TemporaryData::where('identifier',$request->identifier)->first();
         if(isset($temporary_data->data->payment_gateway)){
-            return Response::error(['Data is already stored.'],[],404);
+            return Response::error(['Data is already stored.'],[
+                'temporary_data'    => $temporary_data
+            ],404);
         }
         $validated          = $validator->validate();
         $currency           = PaymentGatewayCurrency::where('id',$validated['payment_gateway'])->first();
