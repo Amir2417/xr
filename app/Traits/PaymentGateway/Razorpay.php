2d357cae6ec1272c4c5afacbab43f205cf1196f1
AMIRSOHELAKASH <akash@appdevs.net>
2024-02-20 16:28:12 +0600

    :razor pay update

--- app/Traits/PaymentGateway/Razorpay.php
+++ app/Traits/PaymentGateway/Razorpay.php
@@ -93,14 +93,15 @@ trait Razorpay  {
        
         $data = $temp_data->data; // update the data variable 
         $order = $data->razorpay_order;
-        
+        $user = User::where('id',$temp_data->data->creator_id)->first();
+       
         $order_id                   = $order->id;
         $request_credentials        = $this->getRazorpayRequestCredentials($output);
         $output['order_id']         = $order_id;
         $output['key']              = $request_credentials->key_id;
         $output['callback_url']     = $data->callback_url;
         $output['cancel_url']       = $data->cancel_url;
-        $output['user']             = auth()->guard(get_auth_guard())->user();
+        $output['user']             = $user;
        
         return view('payment-gateway.btn-pay.razorpay', compact('output'));
     }
