fe3edfdf2833adfe738239a822a6b74f762cc472
Amir2417 <amirsohelto@gmail.com>
2024-04-29 13:17:17 +0600

    :app service provider update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,5 +30,9 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
         Schema::defaultStringLength(191);
+        if($this->app->environment('production')) 
+        {
+            \URL::forceScheme('https');
+        }
     }
 }
