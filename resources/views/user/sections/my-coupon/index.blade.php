fb0395a5d6461a42d291318252b8196b96549a0e
Amir2417 <amirsohelto@gmail.com>
2024-05-02 15:33:41 +0600

    my coupon page update

--- resources/views/user/sections/my-coupon/index.blade.php
+++ resources/views/user/sections/my-coupon/index.blade.php
@@ -14,36 +14,43 @@
     <div class="body-wrapper">
       <div class="coupon-code-item">
           <div class="row mb-30-none">
+              @if(isset($bonus))
               <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 mb-30">
-                  <div class="coupon-code-area">
-                      <div class="coupon-type">
-                          <h3 class="title">{{ __("New User Bonus") }}</h3>
-                          <div class="transaction-table-container">
-                              <table class="transaction-table">
-                                <thead>
-                                  <tr>
-                                    <th>{{ __("Coupon Name") }}</th>
-                                    <th>{{ __("Maximum Used") }}</th>
-                                    <th>{{ __("Remaining") }}</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                  <tr>
-                                    <td>
-                                      <div class="cupone-name">
-                                        <span id="coupon-text">{{ @$bonus->coupon_name }}</span>
-                                        <i class="las la-copy copy-coupon"></i>
-                                      </div>
-                                    </td>
-                                    <td>{{ @$bonus->new_user_bonus->max_used }}</td>
-                                    <td>{{ @$bonus->new_user_bonus->max_used }}</td>
-                                  </tr>
-                                </tbody>
-                              </table>
-                          </div>                          
-                      </div>
-                  </div>
+                <div class="coupon-code-area">
+                    <div class="coupon-type">
+                        <h3 class="title">{{ __("New User Bonus") }}</h3>
+                        <div class="transaction-table-container">
+                            <table class="transaction-table">
+                              <thead>
+                                <tr>
+                                  <th>{{ __("Coupon Name") }}</th>
+                                  <th>{{ __("Maximum Used") }}</th>
+                                  <th>{{ __("Remaining") }}</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>
+                                    <div class="cupone-name">
+                                      <span id="coupon-text">{{ @$bonus->coupon_name }}</span>
+                                      <i class="las la-copy copy-coupon"></i>
+                                    </div>
+                                  </td>
+                                  <td>{{ @$bonus->new_user_bonus->max_used }}</td>
+                                  <td>{{ @$bonus->new_user_bonus->max_used }}</td>
+                                </tr>
+                              </tbody>
+                            </table>
+                        </div>                          
+                    </div>
+                </div>
               </div> 
+              @else
+              <div class="alert alert-primary text-center">
+                {{ __("Coupon is not available!") }}
+              </div>
+              @endif
+              
           </div>
       </div>
   </div>
