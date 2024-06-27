5d1be5c3bfa5e9b5c70fe82c2ac4c3f4fced832a
Amir2417 <amirsohelto@gmail.com>
2024-06-27 18:24:13 +0600

    :modify support tickets table

---
+++ database/migrations/2024_06_27_121635_add_new_column_on_support_tickets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('support_tickets',function(Blueprint $table){
+            $table->unsignedBigInteger('agent_id')->nullable()->after('user_id');
+
+            $table->foreign('agent_id')->references('id')->on('agents')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
